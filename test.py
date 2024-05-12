563e735312591fa4084114f158f1e54c73690888
Connor McManigal <conmcmac@gmail.com>
2024-05-12 00:32:05 -0700

    i give up on trying to improve

--- test.py
+++ test.py
@@ -1,22 +1,22 @@
-from binpacking import BestFitDec, BestFit, CustomFit1
+from binpacking import FirstFitDec, CustomFit1
 import math
 import random
 
 data = []
 for i in range(100):
-    data.append(round(random.uniform(0.0,0.8), 8))
+    data.append(round(random.uniform(0.0,0.8), 2))
 
-bfd = BestFitDec()
+ffd = FirstFitDec()
 cf1 = CustomFit1()
-waste2 = bfd.measure(data)
+waste2 = ffd.measure(data)
 waste3 = cf1.measure(data)
 print("Optimal # bins:", math.ceil(sum(data)))
 print()
-print("BFD bin assignments:", bfd.bins)
-print("BFD # Bins:", bfd.num_bins)
+print("FFD bin assignments:", ffd.bins)
+print("FFD # Bins:", ffd.num_bins)
 print()
 print("CF1 bin assignments:", cf1.bins)
 print("CF1 # Bins:", cf1.num_bins)
 print()
-print("wastes = ", "BFD:", waste2, "CF1:", waste3)
+print("wastes = ", "FFD:", waste2, "CF1:", waste3)
 
