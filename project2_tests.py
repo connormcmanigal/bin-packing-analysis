0eb2a76cc3d11596356929fd1ed24e673abd5be3
Aaron Mui <148605357+aamui@users.noreply.github.com>
2024-05-17 14:42:55 -0700

    adding csv

--- project2_tests.py
+++ project2_tests.py
@@ -2,14 +2,17 @@ import binpacking
 import random
 import time
 import math
+import csv
+import pandas as pd
+
 
 '''
 The first part of this code will verify functional correctness of all bin packing algorithms.
 '''
 
-random.seed(260)
+# random.seed(260)
 
-names = ["NexFit", "FirstFit", "BestFit", "FirstFitDec", "BestFitDec", "CustomFit1"]#, "CustomFit2"]
+names = ["NextFit", 'FirstFit' , "BestFit", "FirstFitDec", "BestFitDec", "CustomFit1"]#, "CustomFit2"]
 
 packer = [None]*6
 packer[0] = binpacking.NextFit()
@@ -23,38 +26,53 @@ packer[5] = binpacking.CustomFit1()
 DATA_SIZE = 50
 NUM_EXP = 9
 data = []
+algo_df=pd.DataFrame(columns = ['Algos', 'Data Size', 'Waste', 'Runtime'])
+seeds = [1,2,3]
 
+for exp_num in range(3):
+    DATA_SIZE = 50
+    NUM_EXP = 9
+    data = []   
+    random.seed(seeds[exp_num])
+    for j in range(NUM_EXP):
+        print()
+        DATA_SIZE = DATA_SIZE * 2
+        # print("DATA_SIZE:", DATA_SIZE)
+        data = []
+        for i in range(DATA_SIZE):
+            data.append(round(random.uniform(0.0,0.8), 8))
 
-for j in range(NUM_EXP):
-    print()
-    DATA_SIZE = DATA_SIZE * 2
-    print("DATA_SIZE:", DATA_SIZE)
-    data = []
-    for i in range(DATA_SIZE):
-        data.append(round(random.uniform(0.0,0.8), 8))
+        data_save = data.copy()
 
-    data_save = data.copy()
+        # Uniformly distributed data
+        
+        for i in range(len(packer)):
+            packer[i].reset()
+            # print(names[i])
+            data = data_save.copy()
+            start_time = time.perf_counter()
+            packer[i].measure(data)
+            end_time = time.perf_counter()
+            packer[i].times.append(end_time - start_time)
+        print()
+        # print("Uniformly Distributed Random Data Waste: ")
+        for i in range(len(packer)):
+            waste=[]
+            # print(names[i])
+            waste=[f'{names[i], exp_num}', DATA_SIZE,packer[i].waste[0], packer[i].times[0]]
+            algo_df = algo_df._append(pd.Series(waste, index=algo_df.columns), ignore_index=True)
+            # print(names[i], packer[i].waste)
 
-    # Uniformly distributed data
+        print()
+
+        # print("Uniformly Distributed Random Data Time: ")
+        # for i in range(len(packer)):    
+            # print(names[i], packer[i].times)
     
-    for i in range(len(packer)):
-        packer[i].reset()
-        # print(names[i])
-        data = data_save.copy()
-        start_time = time.perf_counter()
-        packer[i].measure(data)
-        end_time = time.perf_counter()
-        packer[i].times.append(end_time - start_time)
-    print()
-    print("Uniformly Distributed Random Data Waste: ")
-    for i in range(len(packer)):
-        print(names[i], packer[i].waste)
+print(algo_df)
+algo_df.to_csv('binpacking_results.csv', index = False)
 
-    print()
-
-    print("Uniformly Distributed Random Data Time: ")
-    for i in range(len(packer)):    
-        print(names[i], packer[i].times)
+print('done')
 
 # Please read all of the following before starting your implementation:
 #
